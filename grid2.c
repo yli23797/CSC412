c0942cc91094cad0abb3461b972c69a28aeb6917
Marty Moradian <metaphorshearllc@gmail.com>
2016-09-22 18:11:14 -0500

    Rearranged code in grid2.c into functions.

--- grid2.c
+++ grid2.c
@@ -1,120 +1,137 @@
+/**
+
+Diana Lewis, Yanlin Li, Marty Moradian, Stephen Chilcote, Alexander Houser
+
+ **/
+
 #include <stdio.h>
 
-int main()
-{
-	int gnum = 7;
-	int grid[gnum][gnum];
-	int grid2[gnum][gnum];
-	
-	int i, j, k, num, sum;
-	int num1, num2;
-	
-	for(i=0;i<gnum+1;i++)
-	{
-		grid[0][i] = 0;	
-		grid[i][0] = 0;	
-		grid[gnum][i] = 0;
-		grid[i][gnum] = 0;
-	}	
-	
-	for (i = 1; i < gnum; i++)
-	{
-		for (j = 1; j < gnum; j++)
-		{
-			num = rand()%20;
-			grid[i][j] = num;
-		//	printf("%d", grid[i][j]);
-		}
-	//	printf("\n");
-	}
-	printf("A---\n");
-	for (i = 0; i < gnum + 1; i++)
-	{
-		for (j = 0; j < gnum + 1; j++)
-		{
-			printf(" %2d", grid[i][j]);
-		}
-		printf("\n");
-	}
-for (i = 0; i < gnum +1; i++)
-	{
-		for (j = 0; j < gnum +1; j++)
-		{
-			num = 0;
-			grid2[i][j] = num;
-		//printf("%2d", grid2[i][j]);
-		}
-	//	printf("\n");
-	}
-	
-	printf("GRID 2\n");
-	for (i = 0; i < gnum + 1; i++)
-	{
-		for (j = 0; j < gnum + 1; j++)
-		{
-			printf(" %2d", grid2[i][j]);
-		}
-		printf("\n");
-	}
-
-for(k=0; k<5; k++)
-{
-		
-	for (i = 1; i < gnum; i++)
-	{
-		for (j = 1; j < gnum; j++)
-		{		
-			sum = grid[i-1][j-1] + grid[i-1][j]  + grid[i-1][j+1]  + grid[i][j-1] +grid[i][j+1] +grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j];
-			//printf("sum is %d\n", sum);
-			if(sum %10 == 0)
-			{
-				grid2[i][j] = 0;
-			}
-			else if (sum < 50)
-			{
-				grid2[i][j] = grid[i][j] + 3;
-			}
-			else if (sum > 50)
-			{
-				if(sum > 150)
-				{
-					grid2[i][j] = 1;	
-				}
-				else 
-				{
-					grid2[i][j] = grid[i][j] - 3;
-					
-					if(grid2[i][j]<0)
-					{
-						grid2[i][j] = 0;
-					}
-				}
-				
-			}	
-		}
-	}
-for (i = 1; i < gnum; i++)
-		{
-			for (j = 1; j < gnum; j++)
-			{
-			
-				grid[i][j] = grid2[i][j];
-			//printf("%2d", grid2[i][j]);
-			}
-		//	printf("\n");
-		}	
-	
-	printf("\n\nB---\n");
-	for (i = 0; i < gnum + 1; i++)
-	{
-		for (j = 0; j < gnum + 1; j++)
-		{
-			printf(" %2d", grid[i][j]);
-		}
-		printf("\n");
-	}
-	
+/*
+void zero_perimeter(int size, int grid[][size]){
+    
+    int i;
+    
+    for(i=0; i < size + 1; i++)
+    {
+        grid[0][i] = 0; 
+        grid[i][0] = 0; 
+        grid[size][i] = 0;
+        grid[i][size] = 0;
+    }
 }
+*/
+
+void zero_grid(int size, int grid[][size]){
+	int i, j;
+	for (i = 0; i < size + 1; i++) {
+        for (j = 0; j < size +1; j++) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+
+void print_grid(int size, int grid[][size]){
+    int i, j;
+    for (i = 0; i < size + 1; i++)
+    {
+        for (j = 0; j < size + 1; j++)
+        {
+            printf(" %2d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int sum_neighbors(int i, int j, int size, int grid[][size]) {
+	return grid[i-1][j-1] + grid[i-1][j]  + grid[i-1][j+1]  + grid[i][j-1] +grid[i][j+1] +grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j];
+}
+
+int result(int i, int j, int size, int grid[][size], int sum){
 	
-	return 0;
+	int tmp;
+	if(sum %10 == 0)
+	{
+		return 0;
+	}
+	if (sum < 50)
+	{
+		return grid[i][j] + 3;
+	}
+	if (sum > 50)
+	{
+		if(sum > 150)
+		{
+			return 1;    
+		}
+		else 
+		{
+			tmp = grid[i][j] - 3;
+			if (tmp < 0)
+			{
+				return 0;
+			}
+		}
+					
+	}   
+}
+
+int main() {
+    //size = width/height of the square grid
+    int size = 7;
+    int grid[size][size];
+    int grid2[size][size];
+    
+    int i, j, k, num, sum;
+    int num1, num2;
+    
+    //fill grids with zeroes
+    zero_grid(size, grid);
+    zero_grid(size, grid2);
+    
+    //initialize main grid
+          
+    for (i = 1; i < size; i++)
+    {
+        for (j = 1; j < size; j++)
+        {
+            num = rand()%20;
+            grid[i][j] = num;
+        //  printf("%d", grid[i][j]);
+        }
+    //  printf("\n");
+    }
+    
+    printf("A---\n");
+    print_grid(size, grid);      
+    
+    printf("GRID 2\n");
+    print_grid(size, grid2);
+    
+
+	for(k=0; k<5; k++)
+	{
+		for (i = 1; i < size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{       
+				sum = sum_neighbors(i, j, size, grid);
+				grid2[i][j] = result(i, j, size, grid, sum);
+			}
+		}
+		for (i = 1; i < size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+					
+				grid[i][j] = grid2[i][j];
+				//printf("%2d", grid2[i][j]);
+			}
+				//  printf("\n");
+		}   
+			
+		printf("\n\nB---\n");
+		print_grid(size, grid);
+	}
+    return 0;
 }
