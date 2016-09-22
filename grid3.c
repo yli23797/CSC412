49f86f321186db29b6e8451c2dd58be8d6f4ce10
Marty Moradian <metaphorshearllc@gmail.com>
2016-09-22 18:39:07 -0500

    Applied changes made to grid2.c to grid3.c; added reference output for testing

--- grid3.c
+++ grid3.c
@@ -9,6 +9,59 @@ int grid[gnum][gnum];
 int grid2[gnum][gnum];
 
 
+void print_grid(int size, int grid[][size]){
+    int i, j;
+    for (i = 0; i < size + 1; i++)
+    {
+        for (j = 0; j < size + 1; j++)
+        {
+            printf(" %2d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void zero_grid(int size, int grid[][size]){
+	int i, j;
+	for (i = 0; i < size + 1; i++) {
+        for (j = 0; j < size +1; j++) {
+            grid[i][j] = 0;
+        }
+    }
+}
+
+int sum_neighbors(int i, int j, int size, int grid[][size]) {
+	return grid[i-1][j-1] + grid[i-1][j]  + grid[i-1][j+1]  + grid[i][j-1] +grid[i][j+1] +grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j];
+}
+
+int result(int i, int j, int size, int grid[][size], int sum){
+	int tmp;
+	if(sum %10 == 0)
+	{
+		return 0;
+	}
+	if (sum < 50)
+	{
+		return grid[i][j] + 3;
+	}
+	if (sum > 50)
+	{
+		if(sum > 150)
+		{
+			return 1;    
+		}
+		else 
+		{
+			tmp = grid[i][j] - 3;
+			if (tmp < 0)
+			{
+				return 0;
+			}
+		}				
+	}   
+}
+
+
 void *calc( void *param)
 {
 	long myid;
@@ -19,68 +72,21 @@ void *calc( void *param)
 	int jnum = 7;
 	int sum;
 	
-	if(myid == 1)
-	{
-		i = 1;
-		inum = 2;
-	}
-	else if (myid == 2)
-	{
-		i = 2;
-		inum = 3;
-	}
-	else if (myid == 3)
-	{
-		i = 3;
-		inum = 4;
-	}
-	else if (myid == 4)
-	{
-		i = 4;
-		inum = 5;
-	}
-	else if (myid == 5)
-	{
-		i = 5;
-		inum = 6;
+	
+	int inums[NUM_THREADS];
+	for (i = 1; i <= NUM_THREADS; ++i){
+		inums[i] = i + 1;
 	}
 	
-	else if (myid == 6)
-	{
-		i = 6;
-		inum = 7;
-	}
+	i = myid;
+	inum = inums[myid];
 		
 	for (i; i < inum; i++)
 	{
 		for (j = 1; j < jnum; j++)
 		{		
-			sum = grid[i-1][j-1] + grid[i-1][j]  + grid[i-1][j+1]  + grid[i][j-1] +grid[i][j+1] +grid[i+1][j-1] + grid[i+1][j] + grid[i+1][j+1] + grid[i][j];
-			
-			if(sum %10 == 0)
-			{
-				grid2[i][j] = 0;
-			}
-			else if (sum < 50)
-			{
-				grid2[i][j] = grid[i][j] + 3;
-			}
-			else if (sum > 50)
-			{
-				if(sum > 150)
-				{
-					grid2[i][j] = 1;	
-				}
-				else 
-				{
-					grid2[i][j] = grid[i][j] - 3;
-
-					if(grid2[i][j]<0)
-					{
-						grid2[i][j] = 0;
-					}
-				}
-			}	
+			sum = sum_neighbors(i, j, gnum, grid);
+			grid2[i][j] = result(i, j, gnum, grid, sum);	
 		}
 	}
 			
@@ -94,14 +100,9 @@ int main()
 	void *status;
 	int i, j, num, sum;	
 	
-	for(i=0;i<gnum+1;i++)
-	{
-		grid[0][i] = 0;	
-		grid[i][0] = 0;	
-		grid[gnum][i] = 0;
-		grid[i][gnum] = 0;
-	}	
+	zero_grid(gnum, grid2);	
 	
+	//initialize main grid
 	for (i = 1; i < gnum; i++)
 	{
 		for (j = 1; j < gnum; j++)
@@ -112,23 +113,10 @@ int main()
 	}
 	
 	printf("------ Gen 1 ------\n");
-	for (i = 0; i < gnum + 1; i++)
-	{
-		for (j = 0; j < gnum + 1; j++)
-		{
-			printf(" %2d", grid[i][j]);
-		}
-		printf("\n");
-	}
+	print_grid(gnum, grid);
 	
-	for (i = 0; i < gnum +1; i++)
-	{
-		for (j = 0; j < gnum +1; j++)
-		{
-			num = 0;
-			grid2[i][j] = num;	
-		}
-	}
+	//initialize work grid
+	zero_grid(gnum, grid2);
 	
 	for (t=0; t<NUM_THREADS; t++)
 		{
@@ -150,14 +138,7 @@ int main()
 		}	
 	
 	printf("\n\n------ Gen 2 ------\n");
-	for (i = 0; i < gnum + 1; i++)
-	{
-		for (j = 0; j < gnum + 1; j++)
-		{
-			printf(" %2d", grid[i][j]);
-		}
-		printf("\n");
-	}
+	print_grid(gnum, grid);
 		
 	return 0;
 }
