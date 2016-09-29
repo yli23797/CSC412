f214a1d016d6ff68cba08bd0fc39309b8fe88e74
toastengineer <toastengineer@gmail.com>
2016-09-28 23:25:20 -0400

    Add genverifiedtestdata.py; run it to generate a ton of test files _A * _B = _O, where _ is a number that is the square size of the matrices within.

---
+++ G3/genverifiedtestdata.py
@@ -0,0 +1,37 @@
+"""Creates the directory "/verifiedtestdata" and fills it with matrix files.
+5A * 5B = 5O,
+25A * 25B = 25O, etc...
+
+The number is the square size of the matrices.
+"""
+
+import numpy
+import os
+
+def write_matrix(filename, matrix):
+    """Write a Numpy matrix to disk in the text format we're all standardizing on."""
+    with open(filename, "wt") as thefile:
+        #First we write the dimensions...
+        #BTW, This'll explode if you put something other than a two-dimensional array into it. So, uh, don't.
+        thefile.write("{0} {1}\n".format(*matrix.shape))
+
+        #Then start writing the data itself.
+        for theline in matrix:
+            #Turn each row into a list of strings where each string is a number on that row, then join them with spaces
+            #and write that to the file, followed by a newline.
+            thefile.write( ' '.join( [str(x) for x in theline] ) + "\n")
+
+if __name__=="__main__":
+    try:
+        os.mkdir("verifiedtestdata")
+    except FileExistsError:
+        pass
+    for t in range(5, 100, 5):
+        A = numpy.random.randint(-0x7FFFFFFF, 0x7FFFFFFF, (t, t))
+        B = numpy.random.randint(-0x7FFFFFFF, 0x7FFFFFFF, (t, t))
+        Result = A.dot(B)
+
+        write_matrix(os.path.join("verifiedtestdata", "{0}A".format(t)), A)
+        write_matrix(os.path.join("verifiedtestdata", "{0}B".format(t)), B)
+        write_matrix(os.path.join("verifiedtestdata", "{0}O".format(t)), Result)
+
